6f7ede31dc46fcfa667467fb7dd95e6ea482095f
CraigThomasson <c.a.thomasson@hotmail.co.uk>
2022-12-31 18:15:41 +0000

    add get player socre function

--- media/js/main.js
+++ media/js/main.js
@@ -2,13 +2,14 @@ let deck = []
 let playerHand = []
 let shuffledDeck = []
 let playerTotal = 0
+let ace = false
 
 $("#play").on("click", function() {
-    console.log('playing')
-    deck = creatDeck()
-    shuffledDeck = shuffle(deck)
-    console.log("suffeled deck", shuffledDeck)
-    firstHand(shuffledDeck)
+    console.log('playing');
+    deck = creatDeck();
+    shuffledDeck = shuffle(deck);
+    console.log("suffeled deck", shuffledDeck);
+    firstHand(shuffledDeck);
 })
 
 // creates an array of 52 playing cards 
@@ -20,7 +21,7 @@ function creatDeck() {
 
     for (let suitsCount = 0; suitsCount < suits.length; suitsCount++) {
         for (let valuesCount = 0; valuesCount < values.length; valuesCount++) {
-            deck.push(values[valuesCount] + "-" + suits[suitsCount])
+            deck.push(values[valuesCount] + "-" + suits[suitsCount]);
         }
     }
     return deck
@@ -28,7 +29,7 @@ function creatDeck() {
 
 // fisher-yeats shuffle
 function shuffle(array) {
-    console.log(array)
+    console.log(array);
     for(let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
@@ -39,11 +40,11 @@ function shuffle(array) {
 // deals players first hand
 function firstHand(ShuffeldDeck) {
     for(let i = 0; i < 2; i++) {
-        let card = ShuffeldDeck.pop()
-        console.log("card", card)
-        playerHand.push(card)
+        let card = ShuffeldDeck.pop();
+        console.log("card", card);
+        playerHand.push(card);
     }
-    console.log("player hand", playerHand)
+    console.log("player hand", playerHand);
 
     for(let i = 0; i < playerHand.length; i++) {
         let img = playerHand[i]
@@ -51,5 +52,33 @@ function firstHand(ShuffeldDeck) {
             `<div class="col-3 card playing-card"><img class="card-img" src="media/images/cards/${img}.png" alt=""></div>`
         );
     }
+    getPlayerScore(playerHand);
 }
 
+// calculates the player score from playerHand and updates the total on index.html
+function getPlayerScore(playerHand) {
+    let valuesList = ["K", "Q", "J"];
+    let values = [];
+    for(let i = 0; i < playerHand.length; i++) {
+        let card = playerHand[i];
+        let cardSplit = card.split("-");
+        let cardValue = cardSplit[0];
+        if(valuesList.includes(cardValue)) {
+            cardValue = 10;
+            values.push(cardValue);
+        } else if(cardValue === "A") {
+            cardValue = 11;
+            ace = true
+            values.push(cardValue);
+        } else {
+            cardValue = parseInt(cardValue)
+            values.push(cardValue);
+        }
+        console.log(values)
+        let sum = values.reduce(function(a, b){
+            return a + b;
+        }, 0);
+        console.log(sum)
+        console.log(ace)
+    }
+}
